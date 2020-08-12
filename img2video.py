7004a4278f93460e9ee3499ea4f7d190aebef372
Karobben <591465908@qq.com>
2020-08-12 10:46:32 +0800

    img 2 video

--- img2video.py
+++ img2video.py
@@ -20,15 +20,37 @@ fps = args.fps
 
 import cv2, os
 
-List = os.popen('ls '+File).read().split('\n')[:-1]
-img = cv2.imread(File +"/"+List[0])
+def FD_judge(path):
+    result = ""
+    if len(path) > 1:
+        result = "Files"
+    elif os.path.isdir(path[0]):
+        result = "Directory"
+    elif os.path.isfile(path[0]):
+        result = "File"
+    else:
+        result = "path is incorrect"
+    return result
+
+
+Typ_in = FD_judge(File)
+
+if Typ_in == "Directory":
+    List = os.popen('ls '+File).read().split('\n')[:-1]
+    img = cv2.imread(File +"/"+List[0])
+else:
+    List = File
+    img = cv2.imread(List[0])
 
 size = (len(img[0]),len(img))
 fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
 videowriter = cv2.VideoWriter(OUTPUT,fourcc,fps,size)
 
 for i in List:
-    img = cv2.imread(File +"/"+i)
+    if Typ_in == "Directory":
+        img = cv2.imread(File +"/"+i)
+    else:
+        img = cv2.imread(i)
     videowriter.write(img)
 
 videowriter.release()
